fb9e171365770c931f8037d8dadc5f7985b3101d
Chi Mei Wang <kimmy0929@gmail.com>
2020-08-10 15:55:13 +0900

    components and output

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import Profile from "./Profile";
 
-function App() {
-  const users = [
-    { name: "Kimmy", age: 18, id: 1 },
-    { name: "Shane", age: 32, id: 2 },
-    { name: "Sid", age: 30, id: 3 }
-  ]
+// container components with state made by classes
+// UI components without state made by functions
 
-  return (
-    <div className="App">
-      <h1>Test React App</h1>
-      <Profile users={users} />
-    </div>
-  );
+// function App() {
+//   const users = [
+//     { name: "Kimmy", age: 18, id: 1 },
+//     { name: "Shane", age: 32, id: 2 },
+//     { name: "Sid", age: 30, id: 3 }
+//   ]
+
+//   return (
+//     <div className="App">
+//       <h1>Test React App</h1>
+//       <Profile users={users} />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    users: [
+      { name: "Kimmy", age: 18, id: 1 },
+      { name: "Shane", age: 32, id: 2 },
+      { name: "Sid", age: 30, id: 3 }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Profile users={this.state.users} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- test-app/src/Profile.js
+++ test-app/src/Profile.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-function Profile({ users }) {
+// "users" (as params in functon) is to destructure the props
+// both regular function and  arrow function are okay 
+const Profile = ({ users }) => {
+  // const userList = users.map(user => {
+  //   if (user.age > 20) {
+  //     return (
+  //       <div className="profile" key={user.id}>
+  //         <div>Name: {user.name}</div>
+  //         <div>Age: {user.age}</div>
+  //       </div>
+  //     )
+  //   } else {
+  //     return null
+  //   }
+  // })
   const userList = users.map(user => {
-    return (
+    // condition ? (true) : (false)
+    return user.age > 20 ? (
       <div className="profile" key={user.id}>
         <div>Name: {user.name}</div>
         <div>Age: {user.age}</div>
       </div>
-    )
+    ) : null
   })
   return (
     <div className="user-list">
